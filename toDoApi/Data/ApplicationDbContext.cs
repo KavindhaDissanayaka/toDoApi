38a6eb2c2458f584016203489f92cec2c218fdc7
Kavinda Dissanayaka <92946@fhss.sjp.ac.lk>
2024-09-01 16:37:39 +0530

    toDoModel and connection string added

---
+++ toDoApi/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using toDoApi.Models.Entities;
+
+namespace toDoApi.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        //public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options)
+        //{
+
+        //}
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<toDo>toDos { get; set; }
+    }
+}
---
+++ toDoApi/Models/Entities/toDo.cs
@@ -0,0 +1,9 @@
+﻿namespace toDoApi.Models.Entities
+{
+    public class toDo
+    {
+        public Guid Id { get; set; }
+        public required string ToDoName { get; set; }
+
+    }
+}
